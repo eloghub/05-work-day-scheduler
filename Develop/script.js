47a0fcf00415306ffbffc34ba3b9c3c2f50a76bf
eloggit <elogtechmail@gmail.com>
2021-02-26 23:55:32 -0500

    restructured divs and added classes to align with 24 hour clock. also began creating function to display colors in textareas. need to add past, present and future classes

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,17 @@
-console.log(moment().format()  )
+var hourContainers = $("time-block");
 
 $(document).ready(function(){
-
-    $("#currentDay").text(moment().format("dddd, MMMM Do"))
-  
+  //displays current day in the header
+  $("#currentDay").text(moment().format("dddd, MMMM Do"))
+    //
+  hourContainers.each(function(){
+    console.log($(this).attr("data-hour"));
+    // console.log(hourContainers[0].attr("id"))
   });
+});
+
+//function to change color of timeblocks according to what time it is
+// if (moment() < $(".hour")){
+//   then 
+// }
 
